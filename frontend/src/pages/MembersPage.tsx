1aae4d3fe42362cba873aaf385c12e61e9f4a3c6
Riza Nugraha <rnugraha@gmail.com>
2026-05-05 13:50:07 +0200

    remove unused import

--- frontend/src/pages/MembersPage.tsx
+++ frontend/src/pages/MembersPage.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
 	Table,
